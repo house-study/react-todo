8535d1981957389de5556e44377556b64f19aa5c
yeonilil <gwagseoyeon@gmail.com>
2024-05-23 18:24:04 +0900

    Feat: todo list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import { useState } from 'react';
 import Form from './components/Form';
 import Item from './components/Item';
+import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = todo => {
+    const newTodo = {
+      id: todos.length + 1,
+      todo,
+      isCompleted: false,
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
+  const handleCompleteTodo = (id, isCompleted) => {
+    setTodos(
+      todos.map(todo => (todo.id === id ? { ...todo, isCompleted } : todo)),
+    );
+  };
+
+  const handleDeleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <Form />
+      <Form onAddTodo={handleAddTodo} />
       <ul>
-        <Item />
+        {todos.length === 0 ? (
+          <li>할 일이 없습니다.</li>
+        ) : (
+          todos.map(todo => (
+            <Item
+              key={todo.id}
+              id={todo.id}
+              todo={todo.todo}
+              isCompleted={todo.isCompleted}
+              onCompleteTodo={handleCompleteTodo}
+              onDeleteTodo={handleDeleteTodo}
+            />
+          ))
+        )}
       </ul>
     </div>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,32 +1,34 @@
 import PropTypes from 'prop-types';
+import './Item.css';
 
-function Item({ id, todo, isCompleted, onDeleteTodo, onCompleteTodo }) {
+function Item({ id, todo, isCompleted, onCompleteTodo, onDeleteTodo }) {
   return (
     <li>
-      <input type="checkbox" />
-      <p>{todo}</p>
+      <input
+        type="checkbox"
+        checked={isCompleted}
+        onChange={() => onCompleteTodo(id, !isCompleted)}
+      />
+      <span className="todo">{todo}</span>
       <div>
-        <button type="button">삭제</button>
+        <button
+          className="deleteButton"
+          type="button"
+          onClick={() => onDeleteTodo(id)}
+        >
+          삭제
+        </button>
       </div>
     </li>
   );
 }
 
 Item.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  id: PropTypes.number.isRequired,
   todo: PropTypes.string.isRequired,
   isCompleted: PropTypes.bool.isRequired,
-  /**
-   * 할 일 삭제 함수
-   * @param {number|string} id 삭제할 할 일의 id
-   */
-  onDeleteTodo: PropTypes.func.isRequired,
-  /**
-   * 할 일 완료 함수
-   * @param {number|string} id 완료할 할 일의 id
-   * @param {boolean} isCompleted 완료 여부
-   */
   onCompleteTodo: PropTypes.func.isRequired,
+  onDeleteTodo: PropTypes.func.isRequired,
 };
 
 export default Item;
